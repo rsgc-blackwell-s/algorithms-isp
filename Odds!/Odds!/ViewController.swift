7cb97851616324c262f64e9ea0a9b634d3c21398
rsgc-blackwell-s <student@MacBook-Pro443-Blackwell.local>
2016-01-12 16:55:53 -0500

    Linking sliders and labels to code.
    Using the in-app linking abilities I am connecting the slider and
    labels to my code.

--- Odds!/Odds!/ViewController.swift
+++ Odds!/Odds!/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var percentLabel: UILabel!
+    
+    @IBAction func tipSliderChanged(sender: UISlider) {
+        
+        self.percentLabel.text = "\(Int(sender.value))"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
